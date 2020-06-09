d607272d769349c870fbf043ccc3880c0c628d30
xuxiabb <xuxiabb@hotmail.com>
2020-06-09 23:55:09 +0800

    📦 NEW:

--- README.md
+++ README.md
@@ -4,42 +4,44 @@ Write C code like C++ OOP using Macro.
 
 ## Features
 
-| Macro and Naming Rule                       | Usage                                       |
-| ------------------------------------------- | ------------------------------------------- |
-| ```INTERFACE```                             | Interface                                   |
-| ```ABSTRACT```                              | Abstract Class                              |
-| ```CLASS```                                 | Class                                       |
-| ```CLASS_CTOR, ABSTRACT_CTOR, SUPER_CTOR``` | Constructor                                 |
-| ```CLASS_DTOR, ABSTRACT_DTOR, SUPER_DTOR``` | Destructor                                  |
-| ```NEW, DELETE```                           | New and Delete                              |
-| ```FUNC, METHOD```                          | Public Function Declaration and Definition  |
-| ```_FUNC, _METHOD```                        | Private Function Declaration and Definition |
-| ```VIRTUAL_FUNC, VIRTUAL_METHOD```          | Virtual Function Declaration and Definition |
-| ```INHERIT```                               | Inheritance                                 |
-| ```BIND```                                  | Binding                                     |
-| ```OVERRIDE```                              | Override                                    |
-| ```SUPER, SUPER_2, SUPER_3```               | Super                                       |
-| ```Sub, Sub_2, SUb_3```                     | Sub                                         |
-| ```_this, _base, _THIS```                   | This Pointer                                |
-| ```SWITCH```                                | Sub then Super                              |
-| ```LAMBDA```                                | Lambda                                      |
-| ```Namspace, NAMESPACE```                   | Namespace                                   |
-| ```_, __```                                 | Private                                     |
+| Keywords                                | Usages                                                   |
+| --------------------------------------- | -------------------------------------------------------- |
+| `INTERFACE`                             | Interface                                                |
+| `ABSTRACT`                              | Abstract Class                                           |
+| `CLASS`                                 | Class                                                    |
+| `CLASS_CTOR, ABSTRACT_CTOR, SUPER_CTOR` | Constructor                                              |
+| `CLASS_DTOR, ABSTRACT_DTOR, SUPER_DTOR` | Destructor                                               |
+| `NEW, DELETE`                           | New and Delete                                           |
+| `PSVAR, MEMBER`                         | Public Static Member Variable Declaration and Definition |
+| `FUNC, METHOD`                          | Public Member Function Declaration and Definition        |
+| `_FUNC, _METHOD`                        | Private Member Function Declaration and Definition       |
+| `VFUNC, VBFUNC, VMETHOD`                | Virtual Member Function Declaration and Definition       |
+| `PSFUNC`                                | Public Static Member Function Declaration                |
+| `INHERIT`                               | Inheritance                                              |
+| `BIND`                                  | Binding                                                  |
+| `OVERRIDE`                              | Override                                                 |
+| `SUPER, SUPER_2, SUPER_3`               | Super                                                    |
+| `Sub, Sub_2, SUb_3`                     | Sub                                                      |
+| `SWITCH, _THIS_SW`                      | Sub then Super                                           |
+| `_this, _base, _THIS, _THIS_2, _THIS_3` | This Pointer                                             |
+| `DO`                                    | Fast Do Member Function                                  |
+| `LAMBDA`                                | Lambda                                                   |
+| `Namespace`                             | Namespace                                                |
+| `_`                                     | Private                                                  |
 
 ## C Version
 
-C99, GCC
+C99, GCC 9.3.0+
 
 ## Todo
 
-- List initialize
-- Format names of macro variable
-- Enum class
-- Vector
-- Map
-- String
-- Keyword ```VIRTUAL```
+- [ ] Enum class
+- [ ] Namespace
+- [ ] List initialize
+- [ ] Vector
+- [ ] Map
+- [ ] String
 
 ## Issues
 
-- Can not override virtual destructor
+- Can not override destructor
--- include/pop_c.h
+++ include/pop_c.h
@@ -11,104 +11,115 @@
 // Abstract Class
 #define ABSTRACT(Class)         \
     typedef struct Class Class; \
-    void __##Class##Ctor();     \
-    void __##Class##Dtor();     \
+    void _##Class##Ctor();      \
+    void _##Class##Dtor();      \
     struct Class
 
 // Abstract Constructor
 #define ABSTRACT_CTOR(Class, type_args...) \
-    void __##Class##Ctor(Class *_this, ##type_args)
+    void _##Class##Ctor(Class *_this, ##type_args)
 
 // Abstract Destructor
 #define ABSTRACT_DTOR(Class) \
-    void __##Class##Dtor(Class *_this)
+    void _##Class##Dtor(Class *_this)
 
 // Class
 #define CLASS(Class)            \
     typedef struct Class Class; \
-    void __##Class##Ctor();     \
-    void __##Class##Dtor();     \
-    int __New##Class();         \
-    int __Delete##Class();      \
+    void _##Class##Ctor();      \
+    void _##Class##Dtor();      \
+    int _New##Class();          \
+    int _Delete##Class();       \
     struct Class
 
 // Class Constructor
 #define CLASS_CTOR(Class, type_args...)          \
-    int __New##Class(Class **c_ptr)              \
+    int _New##Class(Class **c_ptr)               \
     {                                            \
         *c_ptr = (Class *)malloc(sizeof(Class)); \
         if (!*c_ptr)                             \
             return 1;                            \
         return 0;                                \
     }                                            \
-    void __##Class##Ctor(Class *_this, ##type_args)
+    void _##Class##Ctor(Class *_this, ##type_args)
 
 // Class Destructor
-#define CLASS_DTOR(Class)         \
-    int __Delete##Class(Class *c) \
-    {                             \
-        if (!c)                   \
-            return 1;             \
-        __##Class##Dtor(c);       \
-        free(c);                  \
-        return 0;                 \
-    }                             \
-    void __##Class##Dtor(Class *_this)
+#define CLASS_DTOR(Class)        \
+    int _Delete##Class(Class *c) \
+    {                            \
+        if (!c)                  \
+            return 1;            \
+        _##Class##Dtor(c);       \
+        free(c);                 \
+        return 0;                \
+    }                            \
+    void _##Class##Dtor(Class *_this)
 
 // New
-#define NEW(Class, c, args...)          \
-    ({                                  \
-        {                               \
-            __New##Class(&c);           \
-            __##Class##Ctor(c, ##args); \
-        }                               \
-        c;                              \
+#define NEW(Class, c, args...)         \
+    ({                                 \
+        {                              \
+            _New##Class(&c);           \
+            _##Class##Ctor(c, ##args); \
+        }                              \
+        (Class *)c;                    \
     })
 
 // Delete
 #define DELETE(Class, c) \
-    (__Delete##Class(c))
+    (_Delete##Class(c))
 
-// Public Function Declaration
-#define FUNC(Class, return_type, func, type_args...) \
-    return_type (*func)(Class * _this, ##type_args)
+//  Class Public Static Member Variable Declaration
+#define PSVAR(Class, type, arg) \
+    extern type Class_arg
 
-// Private Function Declaration
-#define _FUNC(Class, return_type, func, type_args...) \
-    return_type (*_##func)(Class * _this, ##type_args)
+// Class Public Static Member Variable Definition
+#define MEMBER(Class, type, arg) \
+    type Class_arg
 
-// Virtual Function Declaration
-#define VIRTUAL_FUNC(Base, Class, return_type, func, type_args...) \
-    return_type (*func)(Base * *_base, ##type_args)
+// Public Member Function Declaration
+#define FUNC(Class, return_type, Func, type_args...) \
+    return_type (*Func)(Class * _this, ##type_args)
 
-// Public Function Definition
-#define METHOD(Class, return_type, func, type_args...) \
-    return_type Class##func(Class *_this, ##type_args)
+// Public Member Function Definition
+#define METHOD(Class, return_type, Func, type_args...) \
+    return_type Class##Func(Class *_this, ##type_args)
 
-// Private Function Definition
-#define _METHOD(Class, return_type, func, type_args...) \
-    return_type _##Class##_##func(Class *_this, ##type_args)
+// Private Member Function Declaration
+#define _FUNC(Class, return_type, Func, type_args...) \
+    return_type (*_##Func)(Class * _this, ##type_args)
 
-// Virtual Function Definition
-#define VIRTUAL_METHOD(Base, Class, return_type, func, type_args...) \
-    return_type Class##func(Base **_base, ##type_args)
+// Private Member Function Definition
+#define _METHOD(Class, return_type, Func, type_args...) \
+    return_type##Class##_##Func(Class *_this, ##type_args)
 
+// Virtual Member Function Declaration in Base
+#define VBFUNC(Base, return_type, Func, type_args...) \
+    return_type (*Func)(Base * _base, ##type_args)
 
-#define _VIRTUAL
-// Virtual
-#define VIRTUAL /_VIRTUAL/
+// Virtual Member Function Declaration in Class
+#define VFUNC(Base, return_type, Func, type_args...) \
+    ;
 
-// Inherit
+// Virtual Member Function Definition
+#define VMETHOD(Base, Class, return_type, Func, type_args...) \
+    return_type Class##V##Func(Base *_base, ##type_args)
+
+// Public Static Member Function Declaration
+#define PSFUNC(Class, return_type, Func) \
+    extern return_type Class##Func()
+
+// Inheritance
 #define INHERIT(Father) \
     struct Father Father
 
 // Binding
-#define BIND(Class, func) \
-    _this->func = Class##func
+#define BIND(Class, Func) \
+    _this->Func = Class##Func
 
 // Override
-#define OVERRIDE(inheritance, Class, func) \
-    _this->inheritance.func = Class##func
+#define OVERRIDE(inheritance, Class, Func) \
+    _this->inheritance.Func = Class##V##Func
 
 // Super
 #define SUPER(Father, c) \
@@ -116,11 +127,11 @@
 
 // Super 2
 #define SUPER_2(GrandFather, Father, c) \
-    SUPER(GrandFather, SUPER(Father, c)
+    SUPER(GrandFather, SUPER(Father, c))
 
 // Super 3
 #define SUPER_3(GrandGrandFather, GrandFather, Father, c) \
-    SUPER(GrandGrandFather,SUPER(GrandFather, SUPER(Father, c))
+    SUPER(GrandGrandFather, SUPER(GrandFather, SUPER(Father, c)))
 
 // Sub
 #define SUB(Father, Son, c) \
@@ -134,24 +145,41 @@
 #define SUB_3(GrandGrandFather, GrandFather, Father, Son, c) \
     SUB(GrandGrandFather, GrandFather, SUB(GrandFather, Father, SUB(Father, Son, c)))
 
-// Father Ctor
+// Father Constructor Only in CLASS_CTOR Start
 #define SUPER_CTOR(Father, args...) \
-    __##Father##Ctor(SUPER(Father, _this), ##args)
+    _##Father##Ctor(SUPER(Father, _this), ##args)
 
-// Father Dtor
+// Father Destructor Only in CLASS_DTOR End
 #define SUPER_DTOR(Father) \
-    __##Father##Dtor(SUPER(Father, _this))
+    _##Father##Dtor(SUPER(Father, _this))
 
-// This Pointer Only in Virtual Function
-#define _THIS (*_base)
+// Sub then Super Only in Virtual Member Function
+#define SWITCH(DestFather, SrcFather, Son, c) \
+    SUPER(DestFather, SUB(SrcFather, Son, c));
 
-// Sub then Super Only in Virtual Function
-#define SWITCH(DestFather, SrcFather, Son) \
-    SUPER(DestFather, SUB(SrcFather, Son, _THIS));
+// This Pointer Only in Virtual Member Function
+#define _THIS(Father, Son) \
+    Son *_this = SUB(Father, Son, _base)
+
+// This Pointer 2 Only in Virtual Member Function
+#define _THIS(GrandFather, Father, Son) \
+    Son *_this = SUB_2(GrandFather, Father, Son, _base)
+
+// This Pointer 3 Only in Virtual Member Function
+#define _THIS(GrandGrandFather, GrandFather, Father, Son) \
+    Son *_this = SUB_3(GrandGrandFather, GrandFather, Father, Son, _base)
+
+// This Pointer Switch Only in Virtual Member Function
+#define _THIS_SW(DestFather, SrcFather, Son) \
+    Son *_this = SUPER(DestFather, SUB(SrcFather, Son, _base));
+
+// Do Member Function of the Instance and input the Instance
+#define DO(Func, c, args...) \
+    c->Func(c, ##args)
 
 // Lambda
-#define LAMBDA(return_type, function_body) \
+#define LAMBDA(return_type, Function_body) \
     ({                                     \
-        return_type __fn__ function_body   \
+        return_type __fn__ Function_body   \
             __fn__;                        \
     })
